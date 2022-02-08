ebbfb154c8447b295dada00d93e3671ef917e89a
Cacio silva <caciorenato@gmail.comm>
2022-02-07 21:44:15 -0300

    ajuste

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
     "type":"postgres",
     "url":process.env.DATABASE_URL,
+    "extra": {"ssl": { "rejectUnauthorized": false }},
     "migrations":[
         "./dist/database/migrations/*.ts"
     ],
